dc6239f973f39063df4dedf8b387fe95e3fbd05d
harananand27 <haran.anand27@gmail.com>
2016-11-03 16:26:46 +0000

    Add files via upload

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  get 'welcome_page/welcome'
-  root 'welcome_page#welcome'
+
+  root 'posts#index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'posts/index'
+  
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/delete'
+
 end
